0e08ea5249f4b44f3f0a9abf5e08cdae4c941ccf
Josh Jaslow <jbossjaslow@gmail.com>
2021-05-02 11:52:31 -0400

    Initial Commit

---
+++ PocketDex/PocketDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketDexApp.swift
+//  PocketDex
+//
+//  Created by Josh Jaslow on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct PocketDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
